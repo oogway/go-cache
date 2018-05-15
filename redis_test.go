56de3229574513367392762b4f5896523b4c1d7e
Mohan Dutt <mohandutt134@gmail.com>
2018-05-15 20:42:44 +0530

    adding retires in redis.Dial in case of connection error

--- redis_test.go
+++ redis_test.go
@@ -5,9 +5,14 @@
 package cache
 
 import (
+	"os"
 	"net"
 	"testing"
 	"time"
+	"log"
+	"io/ioutil"
+	"regexp"
+	"github.com/bmizerany/assert"
 )
 
 // These tests require redis server running on localhost:6379 (the default)
@@ -61,4 +66,45 @@ func TestRedisCache_GetMulti(t *testing.T) {
 
 func TestRedisCache_Keys(t *testing.T) {
 	testKeys(t, newRedisCache)
+}
+
+// Tests expects to retry redis Dial for defaultRetryThreshold times.
+func TestRedisCache_RetryOnConnectionError(t *testing.T) {
+	// Assuming no tcp process is running on this port on localhost
+	dummyRedisTestServer := "localhost:6666"
+	logFile := "/tmp/testLogFile"
+
+	redisCache := NewRedisCache(RedisOpts{
+		Host:       dummyRedisTestServer,
+		Expiration: time.Duration(1),
+	})
+
+	f, err := os.OpenFile(logFile, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	log.SetOutput(f)
+
+	redisCache.pool.Dial()
+
+	log.SetOutput(os.Stderr)
+	f.Close()
+
+	contents, err := ioutil.ReadFile(logFile)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer os.Remove(logFile)
+
+	r := regexp.MustCompile("Network Error Occured")
+	results := r.FindAllStringIndex(string(contents), -1)
+	assert.Equal(t, defaultRetryThreshold, len(results), string(contents))
+
+	r = regexp.MustCompile("retrying...")
+	results = r.FindAllStringIndex(string(contents), -1)
+	assert.Equal(t, defaultRetryThreshold, len(results), string(contents))
 }
